62584373c50a673c62471db5266b5652bd738d4c
lemuelst2 <123846920+lemuelst2@users.noreply.github.com>
2025-07-20 19:34:06 +0000

    Adicao da peca cavalo

--- xadrez.c
+++ xadrez.c
@@ -2,20 +2,15 @@
 
 int main() {
     // ------------------- TORRE -------------------
-    // A Torre move-se em linha reta horizontal ou vertical.
-    // Simulação: 5 casas para a direita usando 'for'.
     int casasTorre = 5;
     printf("Movimento da Torre (5 casas para a Direita):\n");
     for(int i = 1; i <= casasTorre; i++) {
         printf("Direita\n");
     }
 
-    // Linha separadora para clareza visual
     printf("\n");
 
     // ------------------- BISPO -------------------
-    // O Bispo move-se na diagonal.
-    // Simulação: 5 casas na diagonal para cima e à direita usando 'while'.
     int casasBispo = 5;
     int contadorBispo = 0;
     printf("Movimento do Bispo (5 casas na Diagonal Cima Direita):\n");
@@ -27,8 +22,6 @@ int main() {
     printf("\n");
 
     // ------------------- RAINHA -------------------
-    // A Rainha pode mover-se em todas as direções.
-    // Simulação: 8 casas para a esquerda usando 'do-while'.
     int casasRainha = 8;
     int contadorRainha = 0;
     printf("Movimento da Rainha (8 casas para a Esquerda):\n");
@@ -36,6 +29,26 @@ int main() {
         printf("Esquerda\n");
         contadorRainha++;
     } while(contadorRainha < casasRainha);
+
+    printf("\n");
+
+    // ------------------- CAVALO -------------------
+    int passosVerticais = 2;
+    int passosHorizontais = 1;
+
+    printf("Movimento do Cavalo (em 'L': 2 casas para Baixo, 1 para a Esquerda):\n");
+
+    for(int i = 0; i < passosVerticais; i++) {
+        printf("Baixo\n");
+
+        if (i == passosVerticais - 1) {
+            int j = 0;
+            while(j < passosHorizontais) {
+                printf("Esquerda\n");
+                j++;
+            }
+        }
+    }
 
     return 0;
 }
